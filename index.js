db2b53bb3337371e05d12c708b949ec7bacfed50
Ryan Williams <ryanjacobwilliams@gmail.com>
2019-02-18 20:12:16 -0600

    added scroll bar

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 "use strict";
 
+const $window = $(window);
+const $scrollBar = $("#scroll-bar");
 const $scrollLink = $("#scroll-link");
 const $projects = $(".projects__container");
 
 $scrollLink.on("click", scrollDown);
+$window.on("scroll", scrollBarHighlight);
 
 function scrollDown() {
   $("html,body").animate(
@@ -12,4 +15,13 @@ function scrollDown() {
     },
     800
   );
+}
+
+function scrollBarHighlight() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  $scrollBar.css("width", scrolled + "%");
 }
